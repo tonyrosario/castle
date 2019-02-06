daf45a391256ad6906d78b0373502070493e34aa
Tony Rosario <tony.rosario@grainger.com>
2019-02-06 11:49:37 -0600

    added shell script to add necessary symlinks

---
+++ home/symlinks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# syntax: ln -s target link
+
+ln -s ~/castle/home/.aliases ~/.aliases
+ln -s ~/castle/home/.bash_profile ~/.bash_profile
+ln -s ~/castle/home/.bashrc ~/.bashrc
+ln -s ~/castle/home/.client ~/.client
+
+
+# above creates new symlinks, see below to change/overwrite existing symlinks
+# https://superuser.com/a/938865/29348
